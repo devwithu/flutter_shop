e4dd2aa993b66c2784ff86fafa3e9213b9f4d9a7
devwithu <devwithu@naver.com>
2020-01-13 08:56:03 +0900

    ch 20

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,23 @@ import 'package:flutter_shop/screens/product_detail_screen.dart';
 
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
+import './models/cart.dart';
+
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+                ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
