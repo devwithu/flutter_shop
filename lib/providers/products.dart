1621a5848ead4df172cbd31bd0fd727b6fcb961e
devwithu <devwithu@naver.com>
2020-01-13 08:24:32 +0900

    ch 17

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -37,10 +37,26 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  // var _showFavoritesOnly = false;
+
+  // void showFavoritesOnly() {
+  //   _showFavoritesOnly = true;
+  //   notifyListeners();
+  // }
+  // void showAll() {
+  //   _showFavoritesOnly = false;
+  //   notifyListeners(); 
+  // }
   List<Product> get items {
+    // if (_showFavoritesOnly) {
+    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
+    // }
     return [..._items];
   }
 
+  List<Product> get favoriteItems {
+    return items.where((prodItem) => prodItem.isFavorite).toList();
+  }
   Product findById(String id) {
     return _items.firstWhere( (prod) => prod.id == id );
   }
--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -6,10 +6,14 @@ import './product_item.dart';
 import '../providers/products.dart';
 
 class ProductGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductGrid(this.showFavs);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10),
