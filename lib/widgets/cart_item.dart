933466997b0a6d2d6680c51d1af274aa9fbfd66f
devwithu <devwithu@naver.com>
2020-01-14 10:05:42 +0900

    ch 24

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -5,12 +5,14 @@ import '../models/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
   CartItem(
     this.id,
+    this.productId,
     this.price,
     this.quantity,
     this.title,
@@ -18,7 +20,27 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
         margin: EdgeInsets.symmetric(
           horizontal: 15,
           vertical: 4,
@@ -39,7 +61,7 @@ class CartItem extends StatelessWidget {
             trailing: Text('$quantity x'),
           ),
         ),
-      );
-
+      ),
+    );
   }
 }
