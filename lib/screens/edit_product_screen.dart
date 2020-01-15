a069459eecea23b45df386fa13d52a7fe95a20ed
devwithu <devwithu@naver.com>
2020-01-16 07:45:03 +0900

    ch 12

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -43,6 +43,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
     _form.currentState.save();
     print(_editedProduct.title);
   }
@@ -68,10 +72,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(
                   labelText: 'Title',
+
                 ),
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a value';
+                  }
+                  return null;
+
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
