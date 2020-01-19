ff695c6e661ea0a84e74b2f7db5f0a44ffbfa7a7
devwithu <devwithu@naver.com>
2020-01-20 08:10:21 +0900

    ch 7

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,29 +3,42 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> _authenticate(String email, String password, String urlSegment) async {
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBJfO0GkP8Cb69MoQHwLU4ySqt-lJW1mIw';
-    final response = await http.post(url,
-        body: jsonEncode({
-          'email': email,
-          'password': password,
-          'returnSecuretoken': true,
-        }));
-    print(jsonDecode(response.body));
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyBJfO0GkP8Cb69MoQHwLU4ySqt';
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
-    return _authenticate(email, password, 'signUp');
-
+    return _authenticate(email, password, 'signupNewUser');
   }
 
   Future<void> login(String email, String password) async {
-    return _authenticate(email, password, 'signInWithPassword');
+    return _authenticate(email, password, 'verifyPassword');
   }
 }
