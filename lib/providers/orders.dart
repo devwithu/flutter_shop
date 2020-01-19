12992b7f3a81684705f2d4a573dd853fab5f19b0
devwithu <devwithu@naver.com>
2020-01-19 09:56:40 +0900

    ch 21

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/cart.dart';
 
@@ -18,12 +21,32 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(0, OrderItem(
-        id: DateTime.now().toString(),
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = 'https://atable-97192.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+    final response = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+          'id': cp.id,
+          'title': cp.title,
+          'quantity': cp.quantity,
+          'price': cp.price,
+        })
+            .toList(),
+      }),
+    );
+    _orders.insert(
+      0,
+      OrderItem(
+        id: json.decode(response.body)['name'],
         amount: total,
-        dateTime: DateTime.now(),
-        products: cartProducts)
+        dateTime: timestamp,
+        products: cartProducts,
+      ),
     );
     notifyListeners();
   }
